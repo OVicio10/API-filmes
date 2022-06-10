f3514bb5ddbf2cea1fa70b7ef5036082f81e5261
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-06-10 19:58:15 -0300

    [ADD] Get Id

--- main.py
+++ main.py
@@ -19,3 +19,11 @@ base_de_dados = [
 @app.get("/filmes")
 def get_todos_os_filmes():
     return base_de_dados
+
+# Rota Get Id
+@app.get("/filmes/{id_filme}")
+def get_filme_usando_id(id_filme: int):
+    for filme in base_de_dados:
+        if(filme.id == id_filme):
+            return filme
+    return {"Status": 404, "Mensagem": "Filme não encontrado"}
