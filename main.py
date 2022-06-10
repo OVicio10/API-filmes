a3de50fd540b63d0fa09aba78e17f1a858e4680b
Joao Guilherme <joao.guilherme100@hotmail.com>
2022-06-10 19:57:35 -0300

    [ADD] Get All

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ base_de_dados = [
     Filme(id=1,nome="Homem Aranha",genero="Aventura",lancamento="2021"),
     Filme(id=2,nome="Doutor Estranho",genero="Aventura",lancamento="2022")
 ]
+
+# Get All
+@app.get("/filmes")
+def get_todos_os_filmes():
+    return base_de_dados
